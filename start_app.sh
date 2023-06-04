4c72621b988469766beb22c94db2dbf2ebba5a20
Ian Ellis <ellisiana@gmail.com>
2023-06-04 10:20:56 +0100

    added source of ~/.zshrc to get openai api key

--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /Users/ian/.py3flask/bin/activate
+source /Users/ian/.zshenv
 cd /Users/ian/Documents/src/openai/chat.ellisbs.co.uk
 python /Users/ian/Documents/src/openai/chat.ellisbs.co.uk/app.py
 
