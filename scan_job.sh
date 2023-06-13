63a366e38de2549e0922dc9e189c7085ea720d7f
Ian Ellis <ellisiana@gmail.com>
2023-06-13 23:32:32 +0100

    replaced sonarqube token with masked variable

--- scan_job.sh
+++ scan_job.sh
@@ -1,2 +1,2 @@
 rm -rf __pycache__
-docker run -e SONAR_HOST_URL="https://sonarqube.ellisbs.co.uk" -e SONAR_LOGIN="sqp_3e15c2713c0427caeea973d7001e2346de5529a4" -v "$(pwd):/usr/src" sonarsource/sonar-scanner-cli
+docker run -e SONAR_HOST_URL="https://sonarqube.ellisbs.co.uk" -e SONAR_LOGIN="${SONARQUBE_TOKEN}" -v "$(pwd):/usr/src" sonarsource/sonar-scanner-cli
