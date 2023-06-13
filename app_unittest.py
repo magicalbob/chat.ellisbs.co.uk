379923dc54abe016b59e6d93fb16ae8d867d2df0
Ian Ellis <ellisiana@gmail.com>
2023-06-13 23:32:16 +0100

    improve app unittest

--- app_unittest.py
+++ app_unittest.py
@@ -6,6 +6,8 @@ from flask import Flask
 import app
 import sqlite3
 
+TEST_QUESTION = 'Test question'
+
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = app.app.test_client()
@@ -18,37 +20,31 @@ class TestApp(unittest.TestCase):
 
     @patch('app.openai.ChatCompletion.create')
     @patch('app.sqlite3.connect')  # Stub the database connection
-    def test_ask_route(self, mock_sqlite3_connect, mock_chat_completion_create):
+    def test_ask_route(self, mock_chat_completion_create, mock_sqlite3_connect):
         mock_db = MagicMock()
-        mock_db.cursor.return_value.execute.return_value = None
         mock_sqlite3_connect.return_value = mock_db
-
         mock_response = MagicMock()
         mock_response.choices = [MagicMock()]
         mock_response.choices[0].message = {'content': 'Answer'}  # Update the message structure
         mock_chat_completion_create.return_value = mock_response
-
-        response = self.app.post('/ask', json={'question': 'Test question'})
+        response = self.app.post('/ask', json={'question': TEST_QUESTION})
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Test question', response.data)
+        self.assertIn(bytes(TEST_QUESTION, 'utf-8'), response.data)
         self.assertIn(b'Answer', response.data)
-
         mock_db.commit.assert_called_once()
 
     @patch('app.sqlite3.connect')  # Stub the database connection
     def test_ask_route_db_error(self, mock_sqlite3_connect):
         mock_sqlite3_connect.side_effect = sqlite3.OperationalError
-
-        response = self.app.post('/ask', json={'question': 'Test question'})
+        response = self.app.post('/ask', json={'question': TEST_QUESTION})
         self.assertEqual(response.status_code, 500)
+
 
     @patch('app.sqlite3.connect')  # Stub the database connection
     def test_chat_history_route(self, mock_sqlite3_connect):
         mock_db = MagicMock()
-        mock_db.cursor.return_value.fetchall.return_value = [('Test question', 'Answer', '2023-06-03 12:34:56')]
-
+        mock_db.cursor.return_value.fetchall.return_value = [(TEST_QUESTION, 'Answer', '2023-06-03 12:34:56')]
         mock_sqlite3_connect.return_value = mock_db
-
         response = self.app.get('/chat_history')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<p><strong>', response.data)
