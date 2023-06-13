ed13e74e9e20facb5dc21015106cf769dc8a9ada
Ian Ellis <ellisiana@gmail.com>
2023-06-13 21:15:13 +0100

    update python tests

--- app_unittest.py
+++ app_unittest.py
@@ -2,7 +2,6 @@
 import unittest
 from unittest.mock import patch, MagicMock
 from flask import Flask
-
 import app
 
 class TestApp(unittest.TestCase):
@@ -16,8 +15,9 @@ class TestApp(unittest.TestCase):
         self.assertIn(b'<input id="question-input" type="text" placeholder="Ask a question...">', response.data)  # Update other expected HTML elements
 
     @patch('app.openai.ChatCompletion.create')
+    @patch('app.insert_question_answer')
     @patch('app.sqlite3.connect')  # Stub the database connection
-    def test_ask_route(self, mock_sqlite3_connect, mock_chat_completion_create):
+    def test_ask_route(self, mock_sqlite3_connect, mock_insert_question_answer, mock_chat_completion_create):
         mock_db = MagicMock()
         mock_db.cursor.return_value.execute.return_value = None
         mock_sqlite3_connect.return_value = mock_db
@@ -32,19 +32,24 @@ class TestApp(unittest.TestCase):
         self.assertIn(b'Test question', response.data)
         self.assertIn(b'Answer', response.data)
 
+        # Check if the function for inserting the question and answer into the database was called with correct parameters
+        mock_insert_question_answer.assert_called_with('Test question', 'Answer')
+
     @patch('app.sqlite3.connect')  # Stub the database connection
     def test_chat_history_route(self, mock_sqlite3_connect):
         mock_db = MagicMock()
         mock_db.cursor.return_value.fetchall.return_value = [('Test question', 'Answer', '2023-06-03 12:34:56')]
-
         mock_sqlite3_connect.return_value = mock_db
 
         response = self.app.get('/chat_history')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'<p><strong>', response.data)
-        self.assertIn(b'<p>', response.data)
-        self.assertIn(b'<hr>', response.data)
+        self.assertIn(b'Test question', response.data)
+        self.assertIn(b'Answer', response.data)
+        self.assertIn(b'2023-06-03 12:34:56', response.data)
+
+    def test_ask_route_without_question(self):
+        response = self.app.post('/ask', json={})
+        self.assertEqual(response.status_code, 400)  # Should return 400 Bad Request
 
 if __name__ == '__main__':
     unittest.main()
-
