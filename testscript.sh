4e17f992e449c24953940f7c4c8754aa154ef1ad
Ian Ellis <ellisiana@gmail.com>
2023-06-28 22:41:04 +0100

    corrected chat app coverage

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
 ./app_unittest.py
-~/.local/bin/coverage run -m unittest ctime_blank_unittest.py
+~/.local/bin/coverage run -m unittest app_unittest.py
 ~/.local/bin/coverage xml
 rm -rf __pycache__
