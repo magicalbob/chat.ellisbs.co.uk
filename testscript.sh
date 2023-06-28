e210cf1c766785f1bf1224f3a75236bd66e60e21
Ian Ellis <ellisiana@gmail.com>
2023-06-28 22:28:08 +0100

    reworked for new runner

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,5 @@
-cd /opt/pwd
 pip install -r requirements.txt
 ./app_unittest.py
-/home/appuser/.local/bin/coverage run -m unittest app_unittest.py
-/home/appuser/.local/bin/coverage xml
+~/.local/bin/coverage run -m unittest ctime_blank_unittest.py
+~/.local/bin/coverage xml
 rm -rf __pycache__
