eae5938476e10f20603a93cf871f37ec331e6537
Ian Ellis <ellisiana@gmail.com>
2024-11-20 18:27:06 +0000

    just coverage of app.py not running in itself

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
+set -e  # Exit immediately on error
+
+# Install dependencies
 pip install -r requirements.txt
-./app_unittest.py
-coverage run -m unittest app_unittest.py
+
+# Run tests and generate coverage reports
+python3 -m unittest discover -s . -p "app_unittest.py"
+coverage run -m unittest discover -s . -p "app_unittest.py"
 coverage xml
+
+# Cleanup
 rm -rf __pycache__
